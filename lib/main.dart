5e75bef1a302ba95d138707c0713af7a541d5cff
Khalil <khalilhenane@hotmail.com>
2025-06-19 22:22:36 +0200

    change pipelines branches

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main()  async {
-
+Future<void> main() async {
   //!! It can't find the different env find it out later
   // const env = String.fromEnvironment('env', defaultValue: 'dev');
 
   // await dotenv.load(fileName: '.env');
-  runApp( const MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-    const MainApp({super.key});
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),    
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
