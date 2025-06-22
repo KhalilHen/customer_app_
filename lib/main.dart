e6067b48d71f95a33314c13af3bd86717a7b929f
Khalil <khalilhenane@hotmail.com>
2025-06-22 16:42:52 +0200

    Adjusted the main to load 2 different .env depended on which you have so either prod/dev.  Added supabase to it

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hf_customer_app/pages/homepage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  //!! It can't find the different env find it out later
-  // const env = String.fromEnvironment('env', defaultValue: 'dev');
+  WidgetsFlutterBinding.ensureInitialized();
+  const bool isProd = bool.fromEnvironment('dart.vm.product');
 
-  // await dotenv.load(fileName: '.env');
-  runApp(const MainApp());
+  if (isProd) {
+    await dotenv.load(fileName: ".env.prod");
+  } else {
+    await dotenv.load(fileName: ".env.dev");
+  }
+
+  final showDebugBanner = dotenv.env['SHOW_DEBUG_BANNER'] == 'true';
+
+  final supabaseUrl =
+      dotenv.env['SUPABASE_URL'] ??
+      (throw Exception('Something went wrong with the database!'));
+  final supabaseAnonKey =
+      dotenv.env['SUPABASE_ANON_KEY'] ??
+      (throw Exception('Something went wrong with the database!'));
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  print(showDebugBanner);
+  runApp(MainApp(showDebugBanner: showDebugBanner));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.showDebugBanner});
+  final bool showDebugBanner;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: showDebugBanner,
+
+      home: const Homepage(),
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+
+body: Center(
+  child: Text("Test"),
+),
+
+    );
+  }
+}
