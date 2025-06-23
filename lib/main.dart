257ead019c967bfaea4bcbb86f28b793a9ffe822
Khalil <khalilhenane@hotmail.com>
2025-06-23 16:34:03 +0200

    comment out unsude import and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hf_customer_app/pages/homepage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
